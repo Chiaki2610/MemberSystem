79c5c12a186ee15fb596450e0b36b4812a8b412a
Chiaki <dark0423@hotmail.com>
2025-01-03 02:51:37 +0800

    建立PermissionServiceFactory

---
+++ MemberSystem.ApplicationCore/Factories/PermissionServiceFactory.cs
@@ -0,0 +1,32 @@
+﻿using MemberSystem.ApplicationCore.Interfaces.Services;
+using MemberSystem.ApplicationCore.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberSystem.ApplicationCore.Factories
+{
+    public class PermissionServiceFactory : IPermissionServiceFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public PermissionServiceFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IPermissionService GetService(string type)
+        {
+            return type switch
+            {
+                "Role" => _serviceProvider.GetService<RolePermissionService>(),
+                "Position" => _serviceProvider.GetService<PositionPermissionService>(),
+                _ => throw new ArgumentException("Invalid service type")
+            };
+        }
+    }
+
+}
