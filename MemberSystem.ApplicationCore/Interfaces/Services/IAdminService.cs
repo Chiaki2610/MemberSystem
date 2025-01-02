dbbb528151c826518cb4841893572f2af765d758
Chiaki <dark0423@hotmail.com>
2025-01-03 02:49:28 +0800

    重新歸類IService存放位置

---
+++ MemberSystem.ApplicationCore/Interfaces/Services/IAdminService.cs
@@ -0,0 +1,15 @@
+﻿using MemberSystem.ApplicationCore.Dtos;
+using MemberSystem.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberSystem.ApplicationCore.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<bool> UpdateApprovedAsync(int memberId, bool isApproved);
+    }
+}
